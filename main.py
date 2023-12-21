dbf9fafddd32a62fa38598b56b0632d5fac375a2
Dhruv0x7 <132207676+Dhruv0x7@users.noreply.github.com>
2023-12-21 18:58:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
+import telebot
 from pytube import YouTube
 from pytube.cli import on_progress
+import os
+from keep_alive import keep_alive
 
 
+keep_alive()
 
-link = input("Enter link: ")
-print('Connecting, Please wait...')
+bot = telebot.TeleBot('6800114319:AAFN1thIzE0wydvadH47xO-PZZx9huk19CE')
 
-yt = YouTube(link, on_progress_callback=on_progress)
-vid = yt.streams.get_highest_resolution()
+pwd = os.getcwd()
+print(pwd)
 
-vid.download()
-print("Done!")
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Give url(Ex. https://youtu.be/example) of YT video I download the Video! for you...')
+
+@bot.message_handler(func=lambda message: True)
+def download(message):
+    try:
+        yt = YouTube(message.text, on_progress_callback=on_progress)
+        vid = yt.streams.get_highest_resolution()
+        bot.send_message(message.chat.id, 'Please Wait...')
+        out = vid.download()
+        filename = str(out.split('/')[-1:])
+        size = str(vid.filesize_mb) + ' MB'
+        bitrate = str(vid.bitrate)
+        audio_codec = str(vid.audio_codec)
+        bot.send_message(message.chat.id, 'Filename: ' + filename + '\n' + 'Size: ' + size + '\n' + 'Bitrate: ' + bitrate + '\n' + 'Audio_Codec: ' + audio_codec)
+        video = open(out, 'rb')
+        bot.send_video(message.chat.id, video)
+        bot.send_message(message.chat.id, 'Download Completed!')
+        video.close()
+        os.remove(out)
+    except Exception as e:
+        bot.send_message(message.chat.id, e)
 
 
+bot.infinity_polling()
